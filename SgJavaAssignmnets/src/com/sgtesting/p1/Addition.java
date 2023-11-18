ef3386fd5296ea1a236994e3cebce592c1a9efe6
allurakshith@gmail.com <allurakshith@gmail.com>
2023-11-18 13:08:57 +0530

    Sample Program

---
+++ SgJavaAssignmnets/src/com/sgtesting/p1/Addition.java
@@ -0,0 +1,15 @@
+package com.sgtesting.p1;
+
+public class Addition {
+
+	public static void main(String[] args) {
+
+		// addition of two elements
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Addition Result: "+c);
+	}
+
+}
